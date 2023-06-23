1b547029a526af35f452371f9faef09aa9bda5cd
Irya5 <jainirya5@gmail.com>
2023-06-23 18:08:02 +0530

    dataframe

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+Interi_AR = [
+    {"ID": "1", "NAME": "XYZ", "FABRIC": "XYZ", "TYPE": "XYZ", "ASTHETIC": "Bohemian", "BRAND": ""},
+    {"ID": "2", "NAME": "XYZ", "FABRIC": "XYZ", "TYPE": "XYZ", "ASTHETIC": "Bohemian", "BRAND": ""},
+    {"ID": "3", "NAME": "XYZ", "FABRIC": "XYZ", "TYPE": "XYZ", "ASTHETIC": "Bohemian", "BRAND": ""},
+    {"ID": "4", "NAME": "XYZ", "FABRIC": "XYZ", "TYPE": "XYZ", "ASTHETIC": "Bohemian", "BRAND": ""},
+
+]
+
+dataset_InterAR = pd.DataFrame(Interi_AR)
+print(dataset_InterAR)
+
